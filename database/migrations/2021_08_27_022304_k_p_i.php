b02d96270b14a718c3b34a1b5f27071ed4780026
ạt Nguyễn <nguyenvdat9@gmail.com>
2021-08-29 18:46:50 +0700

    database

--- database/migrations/2021_08_27_022304_k_p_i.php
+++ database/migrations/2021_08_27_022304_k_p_i.php
@@ -14,9 +14,10 @@ class KPI extends Migration
     public function up()
     {
         Schema::create('kpi', function (Blueprint $table) {
+            $table->unsignedInteger('month');
             $table->unsignedInteger('teacher_id');
             $table->unsignedInteger('criteria_id');
-            $table->primary(array('teacher_id','criteria_id'));
+            $table->primary(array('month','teacher_id','criteria_id'));
            $table->foreign('teacher_id')->references('id')->on('teacher');
            $table->foreign('criteria_id')->references('id')->on('criteria');
         });
