d42735b8030c7131620d37908eee55c7cbda7fb6
ạt Nguyễn <nguyenvdat9@gmail.com>
2021-08-29 20:18:53 +0700

    fix

--- public/slug/salary.js
+++ public/slug/salary.js
@@ -1,11 +1,14 @@
 
 $('input#salary_per_hour').keyup(function(event) {
-    let salary_per_hour, percent , salary_ot_per_hour; 
+    let salary_per_hour, percent ; 
     percent = $('#percent').val();
     salary_per_hour = $(this).val();
+    
+    let salary_ot_2 = parseFloat(percent);
+    let salary_ot_1 = parseFloat(salary_per_hour);
     console.log(salary_per_hour);
     console.log(percent);
-    salary_ot_per_hour = salary_per_hour+(percent/100*salary_per_hour);
+    let salary_ot_per_hour = (salary_ot_1+(salary_ot_2/100*salary_ot_1));
       $('input#salary_ot_per_hour').val(Math.round(salary_ot_per_hour));
       return salary_ot_per_hour;
 
